25ea11cdd7edde997c1eb9ae9f57a94f807e47d4
bubbacow2 <bubbacow2@gmail.com>
2025-11-21 17:48:53 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -7,7 +7,7 @@ addLayer("p", {
 		points: new Decimal(0),
     }},
     color: "#596454ff",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    requires: new Decimal(0), // Can be a function that takes requirement increases into account
     resource: "dingus points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
