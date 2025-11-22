51e5eb1b9a8bf61bce2e2e8bc85111c8f2d124dc
bubbacow2 <bubbacow2@gmail.com>
2025-11-21 19:49:15 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -4,10 +4,10 @@ addLayer("p", {
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
-		points: new Decimal(0),
+		points: new Decimal(10),
     }},
     color: "#596454ff",
-    requires: new Decimal(0), // Can be a function that takes requirement increases into account
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "dingus points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
